eb0d1bb1dd513a9829878916a19916443324b1b9
ariel151515 <ndcdigital2020@gmail.com>
2024-03-30 08:15:02 -0300

    ss

--- db/conexion.js
+++ db/conexion.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const password = encodeURIComponent('Relectura1515@@'); 
+require('dotenv').config();
+//const password = encodeURIComponent('Relectura1515@@'); 
 
 // 4.1 or later
-const URI = `mongodb+srv://flextonica:${password}@flextonicacluster.oyhbcld.mongodb.net/?retryWrites=true&w=majority&appName=FlextonicaCluster`;
+//const URI = `mongodb+srv://flextonica:${password}@flextonicacluster.oyhbcld.mongodb.net/?retryWrites=true&w=majority&appName=FlextonicaCluster`;
 
-mongoose.connect(URI, {
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     tlsAllowInvalidCertificates: true, // Habilitar certificados SSL no válidos
 })
 
 module.exports = mongoose.connection; // Exporta la conexión
-
