dd3ec716122df191d088fcf6570b2fd1cdab6609
ariel151515 <ndcdigital2020@gmail.com>
2024-04-23 11:09:41 -0300

    qq

--- models/Objetivo.js
+++ models/Objetivo.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
         type: String,
         unique: false,
     },
-    pesoInicial:{
+    pesoInicial: {
         type: String,
         unique: false,
     },
@@ -17,46 +17,44 @@ const userSchema = new Schema({
         type: String,
         unique: false,
     },
-    tmb:{
-        type: String,
-        unique: false,
+    balanceEnergetico: {
+        tmb: {
+            type: String,
+            unique: false,
+        },
+        superhabit: {
+            type: String,
+            unique: false,
+        },
+        deficit: {
+            type: String,
+            unique: false,
+        },
     },
     porcentaje: {
         type: String,
         unique: false,
     },
-    deficitCalorico:{
-        type: String,
-        unique: false,
-    },
-    superavitCalorico:{
-        type: String,
-        unique: false,
-    },
-    proteinas: {
-        type: String,
-        unique: false,
-    },
-    carbohidratos: {
-        type: String,
-        unique: false,
-    },
-    grasas: {
-        type: String,
-        unique: false,
+    macronutrientes: {
+        proteinas: {
+            type: String,
+            unique: false,
+        },
+        carbohidratos: {
+            type: String,
+            unique: false,
+        },
+        grasas: {
+            type: String,
+            unique: false,
+        },
     },
     margen: {
         type: String,
         unique: false,
-    },
-    factor: {
-        type: String,
-        unique: false,
-    },
+    }
 }, {
     timestamps: true // Habilita los campos createdAt y updatedAt
 });
-
-
 
 module.exports = model('Objetivo', userSchema);
