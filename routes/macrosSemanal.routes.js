09642f98233ae124efd0e63d1e1397ac89ab01eb
ariel151515 <ndcdigital2020@gmail.com>
2024-04-30 13:39:01 -0300

    por body

--- routes/macrosSemanal.routes.js
+++ routes/macrosSemanal.routes.js
@@ -6,8 +6,8 @@ const ctrl = require('../controllers/macrosSemanal.controllers');
 // Crea un documento de macrosSemanal
 router.post('/macrosSemanal/:uid', ctrl.macrosSemanal);
 
-// Trae un documento de macrosSemanal por uid y por fechas
-router.get('/macrossemanal/:uid/:inicioSemana/:finSemana', ctrl.getMacrosSemanal);
+// Trae un documento de macrosSemanal por uid y por fechas (las fechas las pasa por body)
+router.post('/traemacrossemanal/:uid', ctrl.getMacrosSemanal);
 
 
 module.exports = router;
