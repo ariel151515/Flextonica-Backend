04d80596fc438926cbf483e5ef54bf7e88ba42a5
ariel151515 <ndcdigital2020@gmail.com>
2024-05-05 12:37:39 -0300

    rr

--- controllers/macrosSemanal.controllers.js
+++ controllers/macrosSemanal.controllers.js
@@ -58,9 +58,37 @@ exports.getMacrosSemanal = async (req, res) => {
     try {
         const macrosSemanal = await MacrosSemanal.findOne({ uid, fechaInicio: inicioSemana, fechaFin:finSemana});
         if (!macrosSemanal) {
-            return res.status(404).json({ message: 'No se encontró MacrosSemanal para este usuario' });
-        }
+             // Crear un nuevo documento de macrosSemanal
+        const newMacrosSemanal = new MacrosSemanal({ 
+            uid,
+            objetivos: {
+                objetivo: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Objetivo diario de macronutrientes
+                totales: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Totales planeados para la semana
+                restantes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' } // Cantidad restante para alcanzar los objetivos
+            },
+            semana: {
+                lunes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el lunes
+                martes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el martes
+                miercoles: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el miércoles
+                jueves: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el jueves
+                viernes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el viernes
+                sabado: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el sábado
+                domingo: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' } // Datos para el domingo
+            },
+            nota: {
+                contenido: '0'
+            },
+            fechaInicio: inicioSemana,
+            fechaFin: finSemana
+        });
+
+        // Guardar el nuevo documento en la base de datos
+        await newMacrosSemanal.save();
+
+        // Enviar una respuesta al cliente
         res.status(200).json({ macrosSemanal });
+        
+        }
         
     } catch (err) {
         console.log(err);
