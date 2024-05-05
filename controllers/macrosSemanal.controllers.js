3e86ca3578d97cf6503929b682d382e1516f0636
ariel151515 <ndcdigital2020@gmail.com>
2024-05-05 13:02:51 -0300

    rr

--- controllers/macrosSemanal.controllers.js
+++ controllers/macrosSemanal.controllers.js
@@ -56,46 +56,44 @@ exports.getMacrosSemanal = async (req, res) => {
     const { uid, inicioSemana, finSemana } = req.params;
 
     try {
-        const macrosSemanal = await MacrosSemanal.findOne({ uid, fechaInicio: inicioSemana, fechaFin:finSemana});
+        // Busca el documento MacrosSemanal en la base de datos
+        let macrosSemanal = await MacrosSemanal.findOne({ uid, fechaInicio: inicioSemana, fechaFin: finSemana });
+
         if (!macrosSemanal) {
-             // Crear un nuevo documento de macrosSemanal
-        const newMacrosSemanal = new MacrosSemanal({ 
-            uid,
-            objetivos: {
-                objetivo: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Objetivo diario de macronutrientes
-                totales: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Totales planeados para la semana
-                restantes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' } // Cantidad restante para alcanzar los objetivos
-            },
-            semana: {
-                lunes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el lunes
-                martes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el martes
-                miercoles: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el miércoles
-                jueves: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el jueves
-                viernes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el viernes
-                sabado: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }, // Datos para el sábado
-                domingo: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' } // Datos para el domingo
-            },
-            nota: {
-                contenido: '0'
-            },
-            fechaInicio: inicioSemana,
-            fechaFin: finSemana
-        });
+            // Si no se encuentra, crea un nuevo documento de macrosSemanal
+            const newMacrosSemanal = new MacrosSemanal({
+                uid,
+                objetivos: {
+                    objetivo: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    totales: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    restantes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }
+                },
+                semana: {
+                    lunes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    martes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    miercoles: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    jueves: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    viernes: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    sabado: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' },
+                    domingo: { kcal: '0', Carbohidratos: '0', Grasas: '0', Proteinas: '0' }
+                },
+                nota: {
+                    contenido: '0'
+                },
+                fechaInicio: inicioSemana,
+                fechaFin: finSemana
+            });
 
-        // Guardar el nuevo documento en la base de datos
-        await newMacrosSemanal.save();
-
-        // Enviar una respuesta al cliente
-        res.status(200).json({ macrosSemanal });
-        
+            // Guarda el nuevo documento en la base de datos
+            macrosSemanal = await newMacrosSemanal.save();
         }
-         // Enviar una respuesta al cliente
-         res.status(200).json({ macrosSemanal });
-         
+
+        // Envía una respuesta al cliente
+        res.status(200).json(macrosSemanal);
+
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: 'Error al obtener MacrosSemanal' });
-    
+        res.status(500).json({ message: 'Error al obtener o crear macrossemanal' });
     }
 }
 
