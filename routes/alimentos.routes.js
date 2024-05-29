bd35ebb67604d7215da3009dc8d8ac4c72dcf797
ariel151515 <ndcdigital2020@gmail.com>
2024-05-29 16:14:25 -0300

    Describe aquí tu cambio

--- routes/alimentos.routes.js
+++ routes/alimentos.routes.js
@@ -3,16 +3,12 @@ const router = Router();
 
 const ctrl = require('../controllers/alimentos.controllers');
 
-// Crea comidas
 router.post('/creaalimento/', ctrl.creaAlimento);
 
-// Delete comidas
 router.delete('/eliminaalimento/:uid/:alimentoId/', ctrl.eliminaAlimento);
 
-// Editar comidas
 router.put('/editaralimento/:uid/:alimentoId/', ctrl.editaAlimento);
 
-// Trae todos los alimentos
 router.get('/alimentos/:uid', ctrl.getAlimentos);
 
 module.exports = router;
